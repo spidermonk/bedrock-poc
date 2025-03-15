72dfb76fb4d35f9cf24c13272a46b0bc254a180b
spidermonk <jmcginn13@gmail.com>
2025-03-15 17:28:39 -0400

    Initial commit

---
+++ src/main/java/com/xcalibur/bedrock/BedrockPocApplication.java
@@ -0,0 +1,13 @@
+package com.xcalibur.bedrock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedrockPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedrockPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xcalibur/bedrock/BedrockPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcalibur.bedrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedrockPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
