eacc403b6ea40d626f62604237b42a6d19da06c4
spidermonk <jmcginn13@gmail.com>
2025-03-16 17:07:11 -0400

    Add builder to FoundationModel and add get by provider api

--- src/main/java/com/xcalibur/bedrock/models/BedrockModelController.java
+++ src/main/java/com/xcalibur/bedrock/models/BedrockModelController.java
@@ -1,14 +1,18 @@
 package com.xcalibur.bedrock.models;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import software.amazon.awssdk.services.bedrock.BedrockClient;
+import software.amazon.awssdk.services.bedrock.model.FoundationModelSummary;
 import software.amazon.awssdk.services.bedrock.model.ListFoundationModelsRequest;
 import software.amazon.awssdk.services.bedrock.model.ListFoundationModelsResponse;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("bedrock/models")
 public class BedrockModelController {
 
     private final BedrockClient bedrockClient;
@@ -17,13 +21,25 @@ public class BedrockModelController {
         this.bedrockClient = bedrockClient;
     }
 
-    @GetMapping("bedrock/models")
+    @GetMapping("")
     public List<FoundationModel> getBedrockModels() {
 
         ListFoundationModelsResponse response = bedrockClient.listFoundationModels(ListFoundationModelsRequest.builder().build());
-        return response.modelSummaries().stream().map(m ->
-                        new FoundationModel(m.modelArn(), m.modelId(), m.modelName(), m.providerName()))
-                .toList();
+        return response.modelSummaries().stream().map(this::map).toList();
+    }
 
+    @GetMapping("/provider/{provider}")
+    public List<FoundationModel> getBedrockModelsByProvider(@PathVariable("provider") String provider) {
+
+        ListFoundationModelsRequest request = ListFoundationModelsRequest.builder().byProvider(provider).build();
+        ListFoundationModelsResponse response = bedrockClient.listFoundationModels(request);
+        return response.modelSummaries().stream().map(this::map).toList();
+    }
+
+    private FoundationModel map(FoundationModelSummary m) {
+        return FoundationModel.builder().arn(m.modelArn()).id(m.modelId())
+                .name(m.modelName()).provider(m.providerName()).inferenceTypes(m.inferenceTypesSupportedAsStrings())
+                .inputModalities(m.inputModalitiesAsStrings()).outputModalities(m.outputModalitiesAsStrings()).customizationsSupported(m.customizationsSupportedAsStrings())
+                .status(m.modelLifecycle().statusAsString()).streamingSupported(m.responseStreamingSupported()).build();
     }
 }
--- src/main/java/com/xcalibur/bedrock/models/FoundationModel.java
+++ src/main/java/com/xcalibur/bedrock/models/FoundationModel.java
@@ -1,16 +1,24 @@
 package com.xcalibur.bedrock.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class FoundationModel {
 
     private String arn;
     private String id;
     private String name;
     private String provider;
+    private List<String> inferenceTypes;
+    private List<String> inputModalities;
+    private List<String> outputModalities;
+    private List<String> customizationsSupported;
+    private String status;
+    private Boolean streamingSupported;
 }
